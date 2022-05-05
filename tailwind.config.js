3d7ca6f16186446bdc138dbde4e841a85c5503f5
Mariana Agostinelli <marianaagost2@gmail.com>
2022-05-04 22:23:23 -0300

    create and style WidgetForm

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
       colors: {
         brand: {
           500: "#8257e6",
+        },
+        darkSurfacePrimary: {
+          500: "#18181B",
+        },
+        darkSurfaceSecundary: {
+          500: "#27272A",
         }
       }
     },
