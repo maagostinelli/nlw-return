3ec6f6db135b575f45d3e7a47fb1c1e585210a13
Mariana Agostinelli <marianaagost2@gmail.com>
2022-05-09 18:46:30 -0300

    change root folder

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express();
+app.use(express.json());
+
+app.post('/feedbacks', (req,res) => {
+    console.log(req.body);
+    return res.send("Hello World");
+})
+
+app.listen(3333, () => {
+    console.log("Http server running!");
+})
