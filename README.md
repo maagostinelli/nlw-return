dbc50fbbba20b48d0e40c366187de87c89a560b8
Mariana Agostinelli <marianaagost2@gmail.com>
2022-05-09 22:39:17 -0300

    add feedback route and nodemailer config

---
+++ README.md
@@ -0,0 +1,21 @@
+# SOLID
+//conjunto de "regras" de programação criadas no contexto de POO
+
+1. Single Responsability Principle
+    Cada classe/função deve ter responsabilidade úcica
+    (não deve ter mais de uma função)
+
+2. Open/Closed Principle
+    As classes devem ser abertas para extensão mas fechadas para modificação
+    (uma classe não deve mudada, mas pode ter complementação de outra)
+
+3. Liskov Substitution Principle
+    Devemos poder substituir uma classe pai por uma filha e tudo funcionar
+    (uma classe filho pode ter "especialidade", mas não podem ser contraditórias)
+
+4. Interface Segregation Principle
+    Devemos sempre separar e segragar as especialidades de uma classe sempre q possível
+
+5. Dependency Invertion Principle
+    Devemos tratar a entrega de dependecias de uma classe de modo inversivo
+    (uma classe não busca suas dependências por ela mesma, estas informações são passadas a ela pelo contexto)
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,44 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
+import { title } from 'process';
+import { prisma } from './prisma';
 
-const app = express();
-app.use(express.json());
+const app = express(); 
+app.use(express.json()); 
+//necessário pq express não entende json do corpo da requisição
 
-app.post('/feedbacks', (req,res) => {
-    console.log(req.body);
-    return res.send("Hello World");
+var transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "45ec5274bd3fb3",
+      pass: "6d3b70bd3f8b7b"
+    }
+  });
+
+app.post('/feedbacks', async (req,res) => {
+    const {type, comment, screenshot} = req.body;
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot
+            //shortsintax: chave = nome do valor -> não precisa ser escrita
+        }
+    })
+
+    await transport.sendMail({
+        from: 'Feedget Team <oi@feedget.com>',
+        to: 'Mariana A Agostinelli <marianaagost2@gmail.com>',
+        subject: 'Novo Feedback - Feedget',
+        html: `<div style="font-family:sans-serif; color: #111"><p>Tipo de feedback: ${type}</p><p>Comentário: ${comment}</p></div>`
+    });
+
+    return res.status(201).json({ data: feedback });
+    //201 -> código de criação do protocolo http (algo foi criado para o backend)
 })
 
 app.listen(3333, () => {
     console.log("Http server running!");
 })
+//npx prisma studio -> interface nativa para ver banco de dade+os
